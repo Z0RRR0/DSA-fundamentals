b9ddca640dd49d76eed89d9c72a8f1c157990c96
Tushar <tusharbhatt.1405@gmail.com>
2025-08-17 23:13:28 +0530

    Kna[sack and twoSum

---
+++ JavaScript Algorithms - 28 - Misc ProblemsJavaScript Algorithms - 35 - Further Learning/twoSum.js
@@ -0,0 +1,30 @@
+// function twoSum(nums, target) {
+
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if(nums[i] +  nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+
+function twoSum(nums, target) {
+    const numberToIndex = {};
+
+
+    for (let i = 0; i < nums.length; i++) {
+        const numberNeeded = target - nums[i];
+
+        if (numberToIndex[numberNeeded] !== undefined) {
+            return [numberToIndex[numberNeeded], i]
+        }
+
+        
+        numberToIndex[nums[i]] = i;
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3], 6))
